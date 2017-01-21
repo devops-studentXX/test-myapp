ab694aef9d336d04d073de94d987655caabf409d
Jorge Moratilla <jorge@moratilla.com>
2017-01-21 08:00:23 +0100

    changed welcome message

--- src/main/java/es/examplecorp/HelloWorld.java
+++ src/main/java/es/examplecorp/HelloWorld.java
@@ -6,6 +6,6 @@ import static spark.Spark.*;
 public class HelloWorld implements SparkApplication {
     @Override
     public void init() {
-        get("/",(req,res) -> "hello spark");
+        get("/",(req,res) -> "hello DevOps");
     }
 }
