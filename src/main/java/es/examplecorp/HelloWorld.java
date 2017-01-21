b28699fd1aa00238076b6f295682cd8f0a1419b9
Jorge Moratilla <jorge@moratilla.com>
2017-01-21 07:27:05 +0100

    initial commit

---
+++ src/main/java/es/examplecorp/HelloWorld.java
@@ -0,0 +1,11 @@
+package es.examplecorp;
+import spark.servlet.SparkApplication;
+
+import static spark.Spark.*;
+
+public class HelloWorld implements SparkApplication {
+    @Override
+    public void init() {
+        get("/",(req,res) -> "hello spark");
+    }
+}
